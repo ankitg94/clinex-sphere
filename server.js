6ac6acd143881cec9411d5964da8e6cac2f5b1a2
ankit gupta <103044611+ankitg94@users.noreply.github.com>
2024-09-19 11:01:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,19 +6,35 @@ import healthRoute from './Route/healthRoute.js'
 import authRoute from "./Route/authRoute.js"
 import { configDotenv } from "dotenv";
 import { databaseConnection } from "./config/connectDb.js";
-
+import path from 'path'
+import { fileURLToPath } from "url";
 
 configDotenv()
 databaseConnection()
+
+//02-hosting k liye(start)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+// hositng (end)
+
 app.use(cors())
 app.use(json())
 
 
 
+//03-hoisting(start)
+app.use(express.static(path.join(__dirname, './client/build')))
+// hositng (end)
+
 app.use("/api/v1/health",healthRoute)
 app.use("/api/v1/auth",authRoute)
 
 
+//04-hoisting(start)
+app.use("*",function(req,res){
+       res.sendFile(path.join(__dirname,"./client/build/index.html"));
+});
+// hositng (end)
 
 
 const Port = process.env.PORT ||4040 ;
